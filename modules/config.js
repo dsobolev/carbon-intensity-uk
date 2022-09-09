f416324e9a58a17b1debae5bdd093898feb069af
dsobolev <denis.sobolev1@gmail.com>
2022-09-09 22:51:17 +0300

    Pie chart preconfigured

--- modules/config.js
+++ modules/config.js
@@ -1,24 +1,29 @@
 const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
+    'Gas',
+    'Nuclear',
+    'Wind',
+    'Solar',
+    'Others'
 ];
 
 const data = {
     labels: labels,
     datasets: [{
         label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
+        backgroundColor: [
+            '#f3c677',
+            '#b33f62',
+            '#7b1e7a',
+            '#f9564f',
+            '#0c0a3e'
+        ],
+        data: [300, 50, 100, 20, 50],
+        hoverOffset: 4
     }]
 };
 
 export const config = {
-    type: 'line',
+    type: 'pie',
     data: data,
     options: {}
 };
